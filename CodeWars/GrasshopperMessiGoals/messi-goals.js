de6d9c005fe6a02f2c520aac260631f5a7969fa4
mikhailir <saburov1985@gmail.com>
2023-04-27 17:57:31 +1100

    Добавил задачу Messi goals function

---
+++ CodeWars/GrasshopperMessiGoals/messi-goals.js
@@ -0,0 +1,5 @@
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  let result;
+  return (result = laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+}
