434b2fc4fb7573e70dd42a587895ab7864f3c9c6
mikhailir <saburov1985@gmail.com>
2023-04-27 18:27:10 +1100

    Добавил задачу ou Cant Code Under Pressure 1

--- CodeWars/BASIC_Making-Six-Toast./sixToast.js
+++ CodeWars/BASIC_Making-Six-Toast./sixToast.js
@@ -1,6 +1,8 @@
-function converter(mpg) {
-  //code to convert miles per imperial gallon to kilometers per liter
-  const mile = 1.609344;
-  const gallon = 4.54609188;
-  return +((mpg * mile) / gallon).toFixed(2);
+function sixToast(num) {
+  if (num < 6) {
+    return 6 - num;
+  } else {
+    return num - 6;
+  }
+  // you code here
 }
